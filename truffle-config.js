3ca2e6cdd1de126f6e77acfa50a5566b9753f4f6
Athul Sanjose <59477486+UltimateRoman@users.noreply.github.com>
2021-08-27 21:55:19 +0530

    Initial Commit

---
+++ truffle-config.js
@@ -0,0 +1,24 @@
+require('babel-register');
+require('babel-polyfill');
+
+module.exports = {
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*"
+    }
+  },
+
+  contracts_directory: './src/contracts/',
+  contracts_build_directory: './src/abis/',
+  compilers: {
+    solc: {
+      version: "0.7.1",
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  }
+}
