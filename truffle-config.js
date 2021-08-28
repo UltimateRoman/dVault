80743ec348deae7ddb06212e59f8da449203179f
Athul Sanjose <59477486+UltimateRoman@users.noreply.github.com>
2021-08-28 11:56:58 +0530

    Integrated Celo testnet

--- truffle-config.js
+++ truffle-config.js
@@ -1,5 +1,17 @@
 require('babel-register');
 require('babel-polyfill');
+const fs = require('fs');
+
+const Kit = require('@celo/contractkit')
+const kit = Kit.newKit('https://alfajores-forno.celo-testnet.org')
+
+const getAccount = require('./getAccount').getAccount
+
+async function awaitWrapper() {
+  let account = await getAccount()
+  kit.connection.addAccount(account.privateKey)
+}
+awaitWrapper()
 
 module.exports = {
   networks: {
@@ -7,6 +19,10 @@ module.exports = {
       host: "127.0.0.1",
       port: 8545,
       network_id: "*"
+    },
+    alfajores: {
+      provider: kit.connection.web3.currentProvider, 
+      network_id: 44787 
     }
   },
 
