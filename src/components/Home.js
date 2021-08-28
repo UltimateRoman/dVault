bcd7e8658bfda7c1365361552626a243623e158c
Athul Sanjose <59477486+UltimateRoman@users.noreply.github.com>
2021-08-28 21:13:10 +0530

    Certificate verification

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,17 @@
 import React from 'react';
 import logo from '../logo.png';
+import { Button } from 'react-bootstrap';
 
 class Home extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            found: "ns",
+            cert: null
+        }
+    }
+
     render() {
         return (
             <React.Fragment>
@@ -11,8 +21,32 @@ class Home extends Component {
                 <h3 style={{textAlign: "center", color: "blue"}}>By Team Galaxy Stars</h3>
                 <br></br>
                 <img src={logo} alt="logo"/>
+                <br/><br/><br/><br/>
+                <h2 style={{textAlign: "center", color: "black", fontSize:"45px"}}>Verify Certificates</h2>
+                <br/>
+                <form onSubmit={async(event) => {
+                        event.preventDefault()
+                        const res = await this.props.verifyCertificate(this.certid.value)
+                        if(res[0]) {
+                            this.setState({ found: "yes" })
+                            const cert = this.props.certificates(res[1])
+                            this.setState({ cert })
+                        }
+                        else {
+                            this.setState({ found: "no" })
+                        }
+                    }}>
+                <div class="mb-3">
+                <input placeholder="Enter Certificate ID to search" id="certid" style={{ width: '80%', margin: 'auto' }} type="text" class="form-control" ref={(input) => { this.certid = input }} />
+                <Button variant="success" type="submit">
+                    Verify
+                </Button>
+                <p>{this.state.found}</p>
 
-                
+                </div>
+                </form>
+                <br/>
+                        
             </React.Fragment>
         );
     }
